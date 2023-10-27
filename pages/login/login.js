a39ac16572ec463ce4e9f4740e0d0a181c67a10c
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2023-10-27 21:24:04 +0800

    feat: 首页页面样式

---
+++ pages/login/login.js
@@ -0,0 +1,21 @@
+// pages/login/login.js
+Page({
+  data: {
+    // 轮播图数据
+    swiperList: [
+      '/images/swiper1.jpg',
+      '/images/swiper2.jpg'
+    ],
+    // 首页背景图片
+    homeBg: '/images/homeBg.jpg',
+    // 首页标题
+    homeTitle: '/images/homeTitle.png'
+  },
+  /**
+   * @description: 微信登录
+   * @return {void}
+   */
+  wxLogin() {
+    // 唤起微信登录
+  }
+})
