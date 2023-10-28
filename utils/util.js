dec79bcfc12ad72c3c7d01a4ebdff11bb585f7a0
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2023-10-28 19:43:59 +0800

    feat: 登录页面逻辑; 微信小程序ApiPromisify化

--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,51 @@ const formatNumber = n => {
   return n[1] ? n : `0${n}`
 }
 
+/**
+ * @description: 微信api接口promisify
+ * @param {string} apiName
+ * @return {function}
+ */
+const promisify = apiName => {
+  return (options, ...params) => {
+    return new Promise((resolve, reject) => {
+      apiName(Object.assign({}, options, { 
+        success: resolve,
+        fail: reject 
+      }), ...params)
+    })
+  }
+}
+
+/**
+ * @description: 批量promisify化
+ * @param {object} apis
+ * @return {object}
+ */ 
+const promisifyAll = (...apis) => {
+  return (apis || [])
+    .map(name => ({
+      name,
+      member: wx[name]
+    }))
+    .filter(t => typeof t.member === 'function')
+    .reduce((r, t) => {
+      r[t.name] = promisify(t.member)
+      return r;
+    }, {});
+}
+
+// 给外部调用的接口
+const wxApi = promisifyAll(
+  'showToast',
+  'login',
+  'showModal',
+  'navigateTo',
+  'switchTab',
+  'authorize'
+)
+
 module.exports = {
-  formatTime
+  formatTime,
+  wxApi
 }
